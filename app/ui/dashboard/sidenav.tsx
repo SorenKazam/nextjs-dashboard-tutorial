697614c3dd20ff2add2177674ef5bf047bb39add
Tiago Guerreiro <46906203+SorenKazam@users.noreply.github.com>
2025-09-17 11:53:04 +0100

    ADD prefetch attribute to test

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import Link from "next/link";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import AcmeLogo from "@/app/ui/acme-logo";
+import { PowerIcon } from "@heroicons/react/24/outline";
 
 export default function SideNav() {
   return (
@@ -9,6 +9,7 @@ export default function SideNav() {
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
+        prefetch={false}
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
